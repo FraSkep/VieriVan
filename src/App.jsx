9ec3f71de34c207d740f66c3c0db77844994bd1d
FraSkep <francescoschepis.fs@gmail.com>
2026-09-02 15:45:52 +0200

    Modifies before going online

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Testimonials from "./components/Testimonials/Testimonials.jsx";
 import Header from "./components/Header/Header.jsx";
 import FinalCTA from "./components/FinalCTA/FinalCTA.jsx";
 import { ParallaxProvider } from "react-scroll-parallax";
-import Form from "./components/Form/Form.jsx";
 import Team from "./components/Team/Team.jsx";
 
 function App() {
